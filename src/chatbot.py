0dbeebec1afbe6a00a7ce0b1b6d53880dd77f8a9
Nam <nam@YahagaMacbookPro.local>
2023-03-16 00:40:57 +0700

    update file structure, testing some code, unit test

---
+++ src/chatbot.py
@@ -0,0 +1,32 @@
+import os
+import openai
+
+
+class ChatApp:
+    '''
+    Create ChatGPT conversation
+    '''
+
+    def __init__(self):
+        # Setting the API key to use the OpenAI API
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.messages = [
+            {
+                "role": "system",
+                "content": "You are a wealth of knowledge."
+            },
+        ]
+
+    def chat(self, message):
+        '''
+        Append the message so it looks like the conversation is continue
+        '''
+        self.messages.append({"role": "user", "content": message})
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                messages=self.messages)
+        # TODO: handling token count
+        self.messages.append({
+            "role": "assistant",
+            "content": response["choices"][0]["message"].content
+        })
+        return response
---
+++ test/test_chatbot.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from dotenv import load_dotenv
+from src import chatbot
+
+
+class Test(TestCase):
+
+    def test_chatgpt3(self):
+        app = chatbot.ChatApp()
+        message_list = [
+            "I want to know how to learn python quickly?",
+            "What are the best resources for this?",
+            "what should I learn first?"
+        ]
+        for m in message_list:
+            res = app.chat(m)
+            print(m, res)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    a = Test()
+    a.test_chatgpt3()
